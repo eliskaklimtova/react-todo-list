8ce439ab44ab9f86456582815dd768ee8c21cad5
Eliska Klimtova <eli.klimtova@gmail.com>
2019-06-04 16:12:28 +0200

    Initial commit

---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class List extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default List;
